49c3a516ff777983c2b45834bdc2e8c67a855eb3
Shabab Noor <57612672+ashababnoor@users.noreply.github.com>
2021-01-07 09:43:39 +0600

    Update Main.java

--- GradeCalculator/src/sample/Main.java
+++ GradeCalculator/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Grade Calculator");
         primaryStage.setScene(new Scene(root, 454, 340));
         primaryStage.show();
     }
